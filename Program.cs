dff51e032875cff546f7f9745df9c6c334140e09
Ahmad Tantowi <ahmadtantowi@outlook.com>
2022-04-04 12:48:40 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using SendGrid;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var sendGridApiKey = app.Configuration.GetSection("SendGrid:ApiKey").Get<string>();
+var sendGridClient = new SendGridClient(sendGridApiKey);
+
+app.MapGet("/email/template", async () =>
+{
+    var queryParams = @"{
+            'generations': 'dynamic',
+            'page_size': 18
+        }";
+    var response = await sendGridClient.RequestAsync(
+        BaseClient.Method.GET,
+        urlPath: "/templates",
+        queryParams: queryParams);
+    var templates = await response.Body.ReadAsStringAsync();
+
+    return Results.Content(templates, "application/json");
+});
+
+app.MapGet("/email/template/{id}", async (string id) =>
+{
+    var response = await sendGridClient.RequestAsync(
+        BaseClient.Method.GET,
+        urlPath: $"/templates/{id}");
+    var templates = await response.Body.ReadAsStringAsync();
+
+    return Results.Content(templates, "application/json");
+});
+
+app.Run();
